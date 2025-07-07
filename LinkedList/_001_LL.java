e38135b041775457e5fb56d82f5f7eff687426f1
Anson Saju George <116173678+Anson-Saju-George@users.noreply.github.com>
2025-07-07 12:20:10 +0530

    Again

--- LinkedList/_001_LL.java
+++ LinkedList/_001_LL.java
@@ -1,53 +1,82 @@
-class _001_LL{
-	Class node{
-	int data;
-	Node next;
-	
-	public Node(int data){
-		this.data=data;
-		this.next=null;
-	}
-	}
+class _001_LL {
 
-	Node head = null, tail=null;
+    class Node {
+        int data;
+        Node next;
 
-	public void insert(int n){
-		Node newnode = new Node();
-		if (head == null){
-			head == newnode;
-			tail = newnode;
-		}
-		else{
-			tail.next = newnode;
-			tail = newnode;
-		}
-	}
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-	public void insert_mid(int pos, int n){
-	Node newnode = new Node(n);
-	if (head == null){
-		head = newnode
-		tail = newnode;
-	}
-	else{
-		Node c = head, p = null;
-		int count=1;
-		if(pos == 1){
-			newnode.next = head;
-			head = newnode;
-			return;
-		}
-	}
-	while (count=pos && c!=null){
-		pc;
-		c=c.next;
-	}
-	
+    Node head = null, tail = null;
 
-	}
+    public void insert(int n) {
+        Node newnode = new Node(n);
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
 
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-		
-	}
+    public void insert_mid(int pos, int n) {
+        Node newnode = new Node(n);
+        if (pos <= 0) {
+            System.out.println("Invalid position.");
+            return;
+        }
+
+        if (head == null || pos == 1) {
+            newnode.next = head;
+            head = newnode;
+            if (tail == null) tail = newnode;
+            return;
+        }
+
+        Node c = head;
+        int count = 1;
+
+        while (count < pos - 1 && c.next != null) {
+            c = c.next;
+            count++;
+        }
+
+        newnode.next = c.next;
+        c.next = newnode;
+
+        if (newnode.next == null) {
+            tail = newnode;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        _001_LL ll = new _001_LL();
+
+        ll.insert(10);
+        ll.insert(20);
+        ll.insert(30);
+        ll.display();  // 10 -> 20 -> 30 -> NULL
+
+        ll.insert_mid(2, 15);
+        ll.display();  // 10 -> 15 -> 20 -> 30 -> NULL
+
+        ll.insert_mid(1, 5);
+        ll.display();  // 5 -> 10 -> 15 -> 20 -> 30 -> NULL
+
+        ll.insert_mid(10, 100);  // Should insert at end
+        ll.display();  // 5 -> 10 -> 15 -> 20 -> 30 -> 100 -> NULL
+    }
 }
